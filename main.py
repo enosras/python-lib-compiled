ed039bd7b9a720050ff558caf365ed84696d61ca
module_dev <module@dev.it>
2025-10-19 11:46:05 -0700

    "made more experiments to master os library/module"

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
 import time
 
-
+def sleepy(timing):
+    #timing = int(input(print("enter preferred sleep duration : ")))
+    time.sleep(timing)
 def print_hi(name):
 
     print(f'Hi, {obunga}')  # Press ⌘F8 to toggle the breakpoint.
@@ -10,10 +12,21 @@ def print_hi(name):
 if __name__ == '__main__':
     obunga = input(print(f'enter name\n'))
     print_hi(obunga)
-    time.sleep(5)
+    timing = int(input(print("enter preferred sleep duration : ")))
+    sleepy(timing)
+    #time.sleep(5)
     print(os.getuid())
-    time.sleep(3)
+    #time.sleep(3)
+  #  timing = int(input(print("enter preferred sleep duration : ")))
+   # sleepy(timing)
     print(os.name)
+    #timing = int(input(print("enter preferred sleep duration : ")))
+   # sleepy(timing)
+    print(os.path)
+    print(os.P_PID)
+    print(os.environ)
+    print(os.getppid())
+
 
 
 
